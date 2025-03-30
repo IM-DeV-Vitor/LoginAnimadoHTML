7e73064c7a9a30b9aa0fa95c3f7f6d627b5b7ee9
DeV-Vitor <vitorhugo1235lll@gmail.com>
2025-03-30 10:02:10 -0400

    Agora sim acabou

--- script.js
+++ script.js
@@ -3,17 +3,46 @@ function clickNaImagem(){
     let infos = document.getElementById("infos")
     let textoBem = document.getElementById("bem")
     let textoVindo = document.getElementById("vindo")
+    if (window.getComputedStyle(infos).display == "none") {
+        imagemAnimada.classList.remove("animadoSaida")
+        
+        setTimeout(() => {
+          textoBem.classList.remove("bemAnimadoSaida")
+          textoVindo.classList.remove("vindoAnimadoSaida")
+        }, 1000)
 
-    imagemAnimada.classList.add("animado")
+        imagemAnimada.classList.add("animado")
 
-    infos.style.display = "block"
-    infos.classList.add("animacaoForm")
-    
-    setTimeout(() =>       
-    {
-    textoBem.classList.add("bemAnimado")
-    textoVindo.classList.add("vindoAnimado")}, 1000
-  )
+        infos.style.display = "block"
+        infos.classList.add("animacaoForm")
+        setTimeout(() =>       
+        {
+        textoBem.classList.add("bemAnimado")
+        textoVindo.classList.add("vindoAnimado")
+      }, 1000)
+      imagemAnimada.style.pointerEvents = "none"
+
+      setTimeout(() => {
+        imagemAnimada.style.pointerEvents = "auto"
+      }, 2700)
+    } else if (window.getComputedStyle(infos).display == "block"){
+        imagemAnimada.classList.remove("animado")
+        infos.classList.remove("animacaoForm")
+
+        imagemAnimada.classList.add("animadoSaida")
+        infos.style.display = "none"
+
+        setTimeout(() => {
+          textoBem.classList.add("bemAnimadoSaida")
+          textoVindo.classList.add("vindoAnimadoSaida")
+        }, 1000)
+
+        imagemAnimada.style.pointerEvents = "none"
+
+        setTimeout(() => {
+          imagemAnimada.style.pointerEvents = "auto"
+        }, 2700)
+    }
 }
 
 function enviarDados() {
